fa8a82743965a6c06d27696874669edc5a103751
Aishwarya Nishad <aishwaryanishad02@gmail.com>
2026-02-12 19:27:47 +0530

    Created All MODELS

---
+++ Backend/models/job.model.js
@@ -0,0 +1,48 @@
+import mongoose from 'mongoose';
+const jobSchema = new mongoose.Schema({
+    title :{
+        type: String, 
+        required: true
+    },
+    description:{
+        type: String,   
+        required: true
+    },
+    requirements: {
+        type: String, 
+        required: true
+     }, 
+     salary :{
+        type: Number,
+        required: true
+     },
+     Locatation:{
+        type: String,
+        required: true
+     },
+     jobType: {
+        type: String,
+        required: true,
+     },
+     postion:{
+        type:Number,
+        required: true
+     },
+     company:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Company',
+        required: true
+     },
+     createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    applications:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Application'
+    }]
+  
+},{timestamps: true});
+
+export const Job = mongoose.model('Job', jobSchema);
